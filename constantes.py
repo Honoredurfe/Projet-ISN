cb686d58409b6cee745f0c1eb3cad069318f7bfd
Ptiot <sleuek@gmail.com>
2016-03-26 11:13:20 +0100

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -1,7 +1,14 @@
+import pygame
 from pygame import *
+from pygame.locals import *
 
 imageTank1 = "tankJoueur1.png"
 imageTank2 = "tankJoueur2.png"
 imageFond = "fond gris 1250.png"
-titreFenetre="Projet ISN TANK 2D"
+titreFenetre="TANK 2D by Carl and Arthur"
+imageMenu="menuV1.png"
 speed = 15
+
+rectangleNiveau1 = pygame.Rect(900,230,1100,330)
+rectangleNiveau2 = pygame.Rect(900,350,1100,450)
+rectangleNiveau3 = pygame.Rect(900,465,1100,565)
