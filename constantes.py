c11808b3d66a337086dee256736224fe1e11f6dd
Ptiot <sleuek@gmail.com>
2016-03-26 12:42:01 +0100

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -7,8 +7,13 @@ imageTank2 = "tankJoueur2.png"
 imageFond = "fond gris 1250.png"
 titreFenetre="TANK 2D by Carl and Arthur"
 imageMenu="menuV1.png"
+imageIcone="tankJoueur1.png"
+imageAides="menuAides.png"
+imageTutoriel = "tutoriel.png"
 speed = 15
 
 rectangleNiveau1 = pygame.Rect(900,230,1100,330)
 rectangleNiveau2 = pygame.Rect(900,350,1100,450)
 rectangleNiveau3 = pygame.Rect(900,465,1100,565)
+rectangleTutoriel = pygame.Rect(160,280,360,380)
+rectangleAides = pygame.Rect(160,400,360,500)
